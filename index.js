dab3348661a51d60aabea6d5c54128e70b8f7501
Arnold Caballlero <arnold.caballero@farmacompara.com.co>
2019-08-20 08:45:57 -0500

    incorporacion de handlebar

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
+// handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req,res) => {
-    res.send('<h1>Hello World</h1>')
+    res.render('index', {
+        title: 'Members App'
+    })
 })
 
 const PORT = process.env.PORT || 5000
